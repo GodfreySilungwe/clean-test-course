fc062cbc7df0b3d72b60cb7d531d1a657f9003d2
GodfreySilungwe <silungwegod@gmail.com>
2026-02-24 18:52:18 +0200

    Added cypress UAT

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,19 @@
 describe('Shows Appeteasers', () => {
   it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+    cy.visit('http://192.168.8.96:3000');
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+    cy.contains('Handshelds').click()
+    cy.contains('Cheese Barger')
+    cy.contains('Fajita Tacos')
   });
   it('selects Appeteasers', () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click()
+    cy.contains('Tater Tots')
+    cy.contains('Buffalo Wings')
+    cy.contains('Cheese Barger').should('not.exist')
+    cy.contains('Fajita Tacos').should('not.exist')
   });
 });
